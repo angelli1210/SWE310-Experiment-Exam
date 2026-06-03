19c2ec565958d71cfbcf71451d73059ec6254a09
angelli1210 <79222834+angelli1210@users.noreply.github.com>
2026-06-03 20:22:15 +0800

    Initial commit: Student Enrollment app (.NET API + React frontend)

---
+++ StudentEnrollment.API/DTO/Students.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Microsoft.Net.Http.Headers;
+using StudentEnrollment.API.Model;
+public class Students
+{
+    int _id = id;
+    string StudentName;
+    string Programme;
+    string EnrollmentStatus;
+
+    [HttpGet]
+    {
+        int id = Id;
+    string studentName;
+    string programme;
+    string enrollmentStatus;
+};
+
+[HttpPost]
+{
+
+}
+;
+
+[HttpPut]
+{
+
+}
+;
+
+
+[HttpDelete]
+{
+
+}
+;
+
+}
